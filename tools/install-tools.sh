3d133bfed4046b54aecb274620d8828c188ad2ef
linux998 <you@example.com>
2019-12-24 18:26:56 +0800

    添加tools和pz目录

---
+++ tools/install-tools.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo && yum makecache
+yum -y install  epel-release yum-utils
+yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+yum -y install  lrzsz unzip zip bash-completion java-1.8.0-openjdk.x86_64 net-tools.x86_64 docker-ce-19.03.5 docker-compose ansible openssh
+
