a316ffff76ef688baa2829ca962c588829fd3b1f
linux998 <you@example.com>
2019-12-25 19:53:18 +0800

    更新install-tools 修正 cp 后的目录

--- tools/install-tools.sh
+++ tools/install-tools.sh
@@ -3,4 +3,4 @@ curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-
 yum -y install  epel-release yum-utils
 yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
 yum -y install  lrzsz unzip zip bash-completion java-1.8.0-openjdk.x86_64 net-tools.x86_64 docker-ce-19.03.5 docker-compose ansible openssh
-cp -r ./lanproxy /opt/ && echo -e "/opt/lanproxy/bin/startup.sh">>/etc/rc.d/rc.local && chmod +x /etc/rc.d/rc.local
+cp -r /system/tools/lanproxy /opt/ && echo -e "/opt/lanproxy/bin/startup.sh">>/etc/rc.d/rc.local && chmod +x /etc/rc.d/rc.local
