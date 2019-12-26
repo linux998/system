bc0427bcdc5d82cd6db925600097221e2c642532
linux998 <you@example.com>
2019-12-26 16:03:09 +0800

    更新intsall-tools 添加tree pstree

--- tools/install-tools.sh
+++ tools/install-tools.sh
@@ -2,5 +2,5 @@
 curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo && yum clean all  &&  yum makecache && yum -y update
 yum -y install  epel-release yum-utils
 yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
-yum -y install  lrzsz unzip zip bash-completion java-1.8.0-openjdk.x86_64 net-tools.x86_64 docker-ce-19.03.5 docker-compose ansible openssh
+yum -y install  lrzsz unzip zip bash-completion java-1.8.0-openjdk.x86_64 net-tools.x86_64 docker-ce-19.03.5 docker-compose ansible openssh tree pstree
 cp -r /system/tools/lanproxy /opt/ && echo -e "/opt/lanproxy/bin/startup.sh">>/etc/rc.d/rc.local && chmod +x /etc/rc.d/rc.local && sh /opt/lanproxy/bin/startup.sh
