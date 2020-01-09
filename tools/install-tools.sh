bd5231cd70a2556bdb90abfcf35883def2adce2d
Li <you@example.com>
2020-01-09 17:55:09 +0800

    修改sh ，关闭update更新

--- tools/install-tools.sh
+++ tools/install-tools.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo && yum clean all  &&  yum makecache && yum -y update
+curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo && yum clean all  &&  yum makecache
+# yum -y update
 yum -y install  epel-release yum-utils
 yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
 yum -y remove docker \
@@ -14,4 +15,4 @@ yum -y remove docker \
                   docker-engine >/dev/null 2>&1
 yum -y install  lrzsz unzip zip bash-completion java-1.8.0-openjdk.x86_64 net-tools.x86_64 docker-ce-19.03.5 docker-compose ansible openssh tree pstree
 chkconfig docker on
-cp -r /system/tools/lanproxy /opt/ && echo -e "/opt/lanproxy/bin/startup.sh">>/etc/rc.d/rc.local && chmod +x /etc/rc.d/rc.local && sh /opt/lanproxy/bin/startup.sh
+\cp -r /system/tools/lanproxy /opt/ && echo -e "/opt/lanproxy/bin/startup.sh">>/etc/rc.d/rc.local && chmod +x /etc/rc.d/rc.local && sh /opt/lanproxy/bin/startup.sh
